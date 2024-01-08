4cf8a851012b5faec94a328ff4a8944f09ecc968
kuntalthakur01 <80209678+Kuntalthakur01@users.noreply.github.com>
2024-01-08 08:19:36 +0530

    Update decathlon.py

--- decathlon.py
+++ decathlon.py
@@ -27,11 +27,11 @@ def scrape_decathlon_product_with_selenium(url):
     options.add_argument("--no-sandbox")  # Fix for sandbox issue on Linux
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")  # Set the window size
-
+    executable_path= './driver/chromedriver'
  
     # Create Chrome WebDriver instance
     #driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome(options=options, executable_path='./driver/chromedriver')
+    driver = webdriver.Chrome(options=options, executable_path=executable_path)
 
 
 
