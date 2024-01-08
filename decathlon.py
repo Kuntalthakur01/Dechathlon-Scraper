3ff08e58e8a901c03769e70a8507228825ebb07d
kuntalthakur01 <80209678+Kuntalthakur01@users.noreply.github.com>
2024-01-08 09:31:34 +0530

    .

--- decathlon.py
+++ decathlon.py
@@ -20,7 +20,7 @@ from selenium.webdriver.chrome.service import Service
 def scrape_decathlon_product_with_selenium(url):
     
 
-    chromium_path = "chrome-linux/chrome"
+    chromium_path = "chrome-mac/Chromium.app/Contents/MacOS/Chromium"
     
     chromedriver_path = "driver/chromedriver"  # Adjust the path accordingly
     service = Service(executable_path=chromedriver_path)
